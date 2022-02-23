0f29a1a9feab4dfd21b805de0b9a2c5335d63c67
NevtBotz <95397046+NevtBotz@users.noreply.github.com>
2022-02-23 14:29:10 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,9 +2,9 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Ilman;;;'
-                    + 'FN:Ilman\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'N:;Nevt;;;'
+                    + 'FN:Nevt\n' // full name
+                    + 'ORG:Nevt;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6281805450249:+62 818-0545-0249\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: '(Owner) Nevt', contacts: [{ vcard }] } }, { quoted: m })
